672e9e2dd64491b88e44aebd9b6c365c991d5afb
Slavco Mitrov <slavco.mitrov@isolutions.mk>
2024-04-19 21:59:49 +0200

    setup store, main Posts component and MainNavLayout component

---
+++ resources/js/Pages/Posts.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MainNavLayout from '@/Layouts/MainNavLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Posts" />
+
+    <MainNavLayout>
+        MainNavLayout
+    </MainNavLayout>
+</template>
