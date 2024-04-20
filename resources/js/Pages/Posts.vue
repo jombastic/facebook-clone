4fdb3fe06e07581da16cd5be5e0ec097c68f72ad
Slavco Mitrov <slavco.mitrov@isolutions.mk>
2024-04-20 10:11:09 +0200

    create the right section of Posts page

--- resources/js/Pages/Posts.vue
+++ resources/js/Pages/Posts.vue
@@ -102,6 +102,53 @@ const user = usePage().props.auth.user;
                 >
                     PostsSection
                 </div>
+
+                <div id="RightSection" class="pl-4 md:block hidden">
+                    <div
+                        class="max-w-[21.25rem] min-w-[15.625rem] mx-auto pt-4"
+                    >
+                        <div
+                            class="flex items-center justify-between border-b border-b-gray-300"
+                        >
+                            <div class="font-semibold">Contacts</div>
+                            <div class="flex items-center">
+                                <template
+                                    v-for="(icon, index) in searchIcons"
+                                    :key="index"
+                                >
+                                    <div
+                                        class="p-2 hover:bg-gray-300 rounded-full cursor-pointer"
+                                    >
+                                        <component
+                                            :is="icon.component"
+                                            :size="icon.size"
+                                            :fillColor="icon.fillColor"
+                                        />
+                                    </div>
+                                </template>
+                            </div>
+                        </div>
+                        <div
+                            class="h-[calc(100vh-7.1875rem)] overflow-auto pt-2"
+                        >
+                            <template v-for="person in names" :key="person.id">
+                                <div
+                                    class="flex items-center justify-start cursor-pointer hover:bg-[#E5E6E9] py-2 rounded-md"
+                                >
+                                    <img
+                                        :src="`https://picsum.photos/id/${person.picId}/300/320`"
+                                        class="rounded-full ml-1 min-w-[2.375rem] max-h-[2.375rem]"
+                                    />
+                                    <div
+                                        class="text-[0.938rem] text-gray-800 font-extrabold pl-3"
+                                    >
+                                        {{ person.name }}
+                                    </div>
+                                </div>
+                            </template>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </MainNavLayout>
