8916919376d90f351b32a7a38d4b2303f937b01b
Slavco Mitrov <slavco.mitrov@isolutions.mk>
2024-04-22 13:21:50 +0200

    create migrations, controllers and models for posts, users and comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'image',
         'email',
         'password',
     ];
@@ -43,5 +44,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 }
