19c03f2c849029dfe41e6b9cbde52c1f8fe9f762
Slavco Mitrov <slavco.mitrov@isolutions.mk>
2024-04-23 15:31:12 +0200

    update the user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\AllPostsCollection;
+use App\Models\Post;
+use App\Models\User;
+use App\Services\ImageService;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class UserController extends Controller
 {
@@ -11,23 +16,10 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+        $posts = Post::where('user_id', auth()->user()->id)->orderBy('created_at', 'desc')->get();
+        return Inertia::render('User', [
+            'posts' => new AllPostsCollection($posts)
+        ]);
     }
 
     /**
@@ -35,30 +27,20 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        $posts = Post::where('user_id', $id)->orderBy('created_at', 'desc')->get();
+        return Inertia::render('User', [
+            'user' => User::find($id),
+            'posts' => new AllPostsCollection($posts),
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function updateImage(Request $request)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $request->validate(['image' => 'required|mimes:jpg,jpeg,png']);
+        $user = (new ImageService)->updateImage(auth()->user(), $request);
+        $user->save();
     }
 }
